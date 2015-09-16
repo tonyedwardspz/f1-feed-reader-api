75855c9b001d5681ed594458b7dc75afab532973
Tony Edwards <tony@plymouthsoftware.com>
2015-09-16 19:03:50 +0100

    Removes uneeded gumpff

--- app/controllers/v1/news_items_controller.rb
+++ app/controllers/v1/news_items_controller.rb
@@ -1,23 +1,6 @@
 class V1::NewsItemsController < ApplicationController
-  require 'crack'
-
-  require 'rubygems'
-  require 'readability'
-  require 'open-uri'
-
   def index
-
-
-
-    # source = open('http://www.bbc.co.uk/sport/0/formula1/34237355').read
-    # content = Readability::Document.new(source).content
-    # content = content.gsub('&#13;', '')
-
-    # puts content
-
-    BbcFeed.fetch_feed
-
-    @news_items = NewsItem.all.limit(30)
+    @news_items = NewsItem.all.order("pub_date DESC").limit(30)
   end
 
 end
