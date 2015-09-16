4b59c315e03e461aaddbbdf93e9570172f6fb93a
Tony Edwards <tony@plymouthsoftware.com>
2015-09-16 23:42:16 +0100

    Moves fecth to a filter action

--- app/controllers/v1/news_items_controller.rb
+++ app/controllers/v1/news_items_controller.rb
@@ -1,6 +1,17 @@
 class V1::NewsItemsController < ApplicationController
+
+  after_filter :fecth, :only => :index
+
   def index
     puts "***************** Index ******************"
     @news_items = NewsItem.all.order("pub_date DESC").limit(30)
   end
+
+
+  private
+    def fetch
+      GetFeed.fetch_bbc_feed
+      GetFeed.fetch_telegraph_feed
+    end
+
 end
