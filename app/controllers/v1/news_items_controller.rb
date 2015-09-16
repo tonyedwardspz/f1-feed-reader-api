91052582680ae707b53ca2dadddf3b7d86f5e55a
Tony Edwards <tony@plymouthsoftware.com>
2015-09-16 23:34:51 +0100

    Double check puts

--- app/controllers/v1/news_items_controller.rb
+++ app/controllers/v1/news_items_controller.rb
@@ -1,5 +1,6 @@
 class V1::NewsItemsController < ApplicationController
   def index
+    puts "***************** Index ******************"
     @news_items = NewsItem.all.order("pub_date DESC").limit(30)
   end
 end
